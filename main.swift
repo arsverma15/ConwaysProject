456329c7c08c9f8bb0404e09c4c8532b73c57f4b
Arshia Verma <mishu45r@gmail.com>
2022-01-10 17:41:09 -0500

    New File

---
+++ main.swift
@@ -0,0 +1,22 @@
+var vc = ColonyVC()
+vc.coordinates()
+
+/*var test = Colony(colonySize:10)
+test.setCellAlive(xCoor: 1, yCoor: 5)
+test.setCellAlive(xCoor: 2, yCoor: 4)
+test.setCellAlive(xCoor: 2, yCoor: 3)
+test.setCellAlive(xCoor: 3, yCoor: 5)
+test.setCellAlive(xCoor: 3, yCoor: 4)
+print(test)
+print("Cells alive: \(test.numberLivingCells)")
+for _ in 0...16{
+    test.evolve()
+}
+print("Many generations later...")
+print(test)
+print("Cells alive: \(test.numberLivingCells)")
+print("Clearing.")
+test.resetColony()
+print(test)
+print("Cells alive: \(test.numberLivingCells)")
+*/
